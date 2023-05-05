ee6bbaa2b1e8aeb28d7e7fc9ee9199791cab8f01
Nahuel <nahuel.profesor@gmail.com>
2023-05-04 23:49:00 -0300

    mostrar quote en pantalla, terminado

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -6,8 +6,7 @@ const fetchQuote = async() => {
 
     const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
     const data = await res.json();
-    
-    console.log(data[0])
+    // console.log(data[0])
     return data[0];
 }   
 
@@ -18,6 +17,20 @@ const fetchQuote = async() => {
 export const BreakingbadApp = async(element) => {
 
     document.querySelector('#app-title').innerHTML = 'Breakingbad App';
-    const quote = await fetchQuote();
-    element.innerHTML = 'Tenemos data ...';
+    // await fetchQuote();
+    
+    const quoteLabel = document.createElement('blockquote');
+    const authoLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = ( data ) => {
+        quoteLabel.innerHTML = data.quote;
+        authoLabel.innerHTML = data.author;
+        element.replaceChildren( quoteLabel, authoLabel, nextQuoteButton );
+    }
+
+    fetchQuote()
+        // .then( data => renderQuote(data) );
+        .then(renderQuote)
 }
