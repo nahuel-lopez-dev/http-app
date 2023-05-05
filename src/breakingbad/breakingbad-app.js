6454f236474bcbafaa5618d87117c975eadd0f0b
Nahuel <nahuel.profesor@gmail.com>
2023-05-04 21:50:28 -0300

    trabajando con la api de breakingbad

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -1,10 +1,23 @@
 
 /**
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async() => {
+
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
+    
+    console.log(data[0])
+    return data[0];
+}   
+
+/**
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingbadApp = (element) => {
+export const BreakingbadApp = async(element) => {
 
     document.querySelector('#app-title').innerHTML = 'Breakingbad App';
-    element.innerHTML = 'Loading...';
+    const quote = await fetchQuote();
+    element.innerHTML = 'Tenemos data ...';
 }
